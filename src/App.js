d01db4aa0bb5b500a7fda81bcf23ef762e3be662
Ishika_Gopie <ishikagopie123@gmail.com>
2025-08-20 08:35:14 -0400

    Add Ant Design (antd) and its dependencies to project
    Integrated Ant Design library (antd v5.27.1) into the project along with required dependencies like `rc-*` components, `classnames`, and `dayjs`. This enables the use of Ant Design components for UI development.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
 
-function App() {
+const { Header, Content, Footer } = Layout;
+
+const items = Array.from({ length: 15 }).map((_, index) => ({
+  key: index + 1,
+  label: `nav ${index + 1}`,
+}));
+
+const App: React.FC = () => {
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Layout>
+        <Header style={{ display: 'flex', alignItems: 'center' }}>
+          <div className="demo-logo" />
+          <Menu
+              theme="dark"
+              mode="horizontal"
+              defaultSelectedKeys={['2']}
+              items={items}
+              style={{ flex: 1, minWidth: 0 }}
+          />
+        </Header>
+        <Content style={{ padding: '0 48px' }}>
+          <Breadcrumb
+              style={{ margin: '16px 0' }}
+              items={[{ title: 'Home' }, { title: 'List' }, { title: 'App' }]}
+          />
+          <div
+              style={{
+                background: colorBgContainer,
+                minHeight: 280,
+                padding: 24,
+                borderRadius: borderRadiusLG,
+              }}
+          >
+            Content
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©{new Date().getFullYear()} Created by Ant UED
+        </Footer>
+      </Layout>
   );
-}
+};
 
 export default App;
