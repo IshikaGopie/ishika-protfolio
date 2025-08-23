1f00210c80e2d30a247a1f34182df804dd52c30c
Ishika_Gopie <ishikagopie123@gmail.com>
2025-08-23 09:41:41 -0400

    updated the skill icons format

--- src/components/molecules/skillsList.jsx
+++ src/components/molecules/skillsList.jsx
@@ -12,31 +12,43 @@ import {
     CIcon,
     SqlIcon,
     GitIcon,
-    GraphIcon
+    GraphIcon,
+    SpringIcon,
+    FastAPIIcon,
+    FlaskIcon,
+    NodeIcon
 } from '../atoms/techIcons';
 
 const SkillsList = ({ size = 100 }) => {
     const skills = [
-        { icon: <HtmlIcon size={size} />, name: 'HTML' },
-        { icon: <CssIcon size={size} />, name: 'CSS' },
-        { icon: <JavaScriptIcon size={size} />, name: 'JavaScript' },
-        { icon: <ReactIcon size={size} />, name: 'React' },
-        { icon: <VueIcon size={size} />, name: 'Vue' },
-        { icon: <PythonIcon size={size} />, name: 'Python' },
-        { icon: <JavaIcon size={size} />, name: 'Java' },
-        { icon: <GoIcon size={size} />, name: 'Go' },
-        { icon: <CIcon size={size} />, name: 'C++' },
-        { icon: <SqlIcon size={size} />, name: 'SQL' },
-        { icon: <GitIcon size={size} />, name: 'Git' },
-        { icon: <GraphIcon size={size} />, name: 'GraphQL' }
+        { icon: <HtmlIcon size={size} />, name: 'HTML', url: 'https://developer.mozilla.org/en-US/docs/Web/HTML' },
+        { icon: <CssIcon size={size} />, name: 'CSS', url: 'https://developer.mozilla.org/en-US/docs/Web/CSS' },
+        { icon: <JavaScriptIcon size={size} />, name: 'JavaScript', url: 'https://developer.mozilla.org/en-US/docs/Web/JavaScript' },
+        { icon: <ReactIcon size={size} />, name: 'React', url: 'https://react.dev' },
+        { icon: <VueIcon size={size} />, name: 'Vue', url: 'https://vuejs.org' },
+        { icon: <PythonIcon size={size} />, name: 'Python', url: 'https://www.python.org' },
+        { icon: <JavaIcon size={size} />, name: 'Java', url: 'https://dev.java' },
+        { icon: <GoIcon size={size} />, name: 'Go', url: 'https://go.dev' },
+        { icon: <CIcon size={size} />, name: 'C++', url: 'https://isocpp.org' },
+        { icon: <SqlIcon size={size} />, name: 'SQL', url: 'https://www.w3schools.com/sql' },
+        { icon: <GitIcon size={size} />, name: 'Git', url: 'https://git-scm.com' },
+        { icon: <GraphIcon size={size} />, name: 'GraphQL', url: 'https://graphql.org' },
+        { icon: <SpringIcon size={size} />, name: 'Spring', url: 'https://spring.io' },
+        { icon: <FastAPIIcon size={size} />, name: 'FastAPI', url: 'https://fastapi.tiangolo.com' },
+        { icon: <FlaskIcon size={size} />, name: 'Flask', url: 'https://flask.palletsprojects.com' },
+        { icon: <NodeIcon size={size} />, name: 'Node JS', url: 'https://nodejs.org' },
     ];
 
     return (
         <div style={{
             display: 'grid',
-            gridTemplateColumns: 'repeat(4, 1fr)',
-            gap: '24px',
+            gridTemplateColumns: 'repeat(auto-fill, minmax(80px, 1fr))',
+            gap: '16px',
             width: '100%',
+            padding: '0 16px',
+            boxSizing: 'border-box',
+            maxWidth: '800px',
+            margin: '0 auto'
         }}>
             {skills.map((skill, index) => (
                 <div key={index} className="skill-item">
