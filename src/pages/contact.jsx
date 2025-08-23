ee48270ae0985a194f9b7a0ef13b9a5f6061ee66
Ishika_Gopie <ishikagopie123@gmail.com>
2025-08-23 12:57:39 -0400

    added emailjs service

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -3,11 +3,31 @@ import ContactForm from '../components/molecules/ContactForm';
 import PersonalInfo from '../components/molecules/PersonalInfo';
 import 'antd/dist/reset.css';
 import './css/contact.css';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
     const handleSubmit = (values) => {
-        console.log('Form values:', values);
-        // Handle form submission here
+        try {
+            const templateParams = {
+                subject: 'New Message from ' + values.firstName + ' ' + values.lastName,
+                firstName: values.firstName,
+                lastName: values.lastName,
+                email: values.email,
+                message: values.message,
+            };
+
+            emailjs.init(process.env.REACT_APP_PUBLIC_KEY);
+            return emailjs.send(
+                process.env.REACT_APP_SERVICE_ID,
+                process.env.REACT_APP_TEMPLATE_ID,
+                templateParams
+            );
+        } catch (error) {
+            console.error('Error sending email:', error);
+            alert('Failed to send message. Please try again.');
+        } finally {
+            alert('Message sent successfully!');
+        }
     };
 
     return (
